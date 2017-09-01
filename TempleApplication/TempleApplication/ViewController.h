e9679ec6ec9d7bd2e9d0f115e2b37e805ca59b8a
HirenSiyaInfo <hiren.patel@siyainfo.com>
2017-09-01 12:01:25 +0530

    Project added to for Temple Application
    Project added to for Temple Application

---
+++ TempleApplication/TempleApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TempleApplication
+//
+//  Created by Siya-ios5 on 9/1/17.
+//  Copyright © 2017 Siya-ios5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TempleApplication/TempleApplication/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TempleApplication
+//
+//  Created by Siya-ios5 on 9/1/17.
+//  Copyright © 2017 Siya-ios5. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
